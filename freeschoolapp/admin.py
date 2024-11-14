362d4ec5f2b31a7c36996a7f14acab12961b6e9d
Nagaoka993 <ymg2365013@stu.o-hara.ac.jp>
2024-11-14 15:19:11 +0900

    あｄみｎ編集

--- freeschoolapp/admin.py
+++ freeschoolapp/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import FreeSchoolUser
-
-#Django管理者サイトにFreeSchoolUserを登録する
-admin.site.register(FreeSchoolUser)
--- studentapp/admin.py
+++ studentapp/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import StudentUser
-
-#Django管理者サイトにStudentUserを登録する
-admin.site.register(StudentUser)
