0f3e5659cecc85e3492e4d72817bd67710ee4cd9
ykt0727 <ymg2365039@stu.o-hara.ac.jp>
2024-11-14 12:38:57 +0900

    フリースクール関係者側のアカウント機能関連の仮のhtmlファイルとルーティング設定が完了しました

--- freeschoolapp/urls.py
+++ freeschoolapp/urls.py
@@ -4,4 +4,15 @@ from . import views
 app_name='freeschoolapp'
 
 #URLパターンを登録するためのリスト
-urlpatterns=[path('',views.IndexView.as_view(),name='index')]
+urlpatterns=[
+    path('login',views.LoginView.as_view(),name='login'),
+    path('top',views.TopView.as_view(),name='top'),
+    path('mailsend',views.MailSendView.as_view(),name='mailsend'),
+    path('mailsenddone',views.MailSendDoneView.as_view(),name='mailsenddone'),
+    path('account',views.AccountView.as_view(),name='account'),
+    path('accountupdate',views.AccountUpdateView.as_view(),name='accountupdate'),
+    path('passwordreset',views.PasswordResetView.as_view(),name='passwordreset'),
+    path('passwordresetdone',views.PasswordResetDoneView.as_view(),name='passwordresetdone'),
+    path('usersearch',views.UserSearchView.as_view(),name='usersearch'),
+    path('userdetail',views.UserDetailView.as_view(),name='userdetail'),
+]
--- freeschoolapp/views.py
+++ freeschoolapp/views.py
@@ -1,7 +1,61 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-class IndexView(TemplateView):
+class LoginView(TemplateView):
     
-    #index.htmlをレンダリング（描写）する
+    #freeschool_login.htmlをレンダリング（描写）する
     template_name='freeschool_login.html'
+
+
+class TopView(TemplateView):
+    
+    #freeschool_top.htmlをレンダリング（描写）する
+    template_name='freeschool_top.html'
+    
+
+class MailSendView(TemplateView):
+    
+    #freeschool_passwordreset.htmlをレンダリング（描写）する
+    template_name='freeschool_mailsend.html'
+    
+
+class MailSendDoneView(TemplateView):
+    
+    #freeschool_mailsenddone.htmlをレンダリング（描写）する
+    template_name='freeschool_mailsenddone.html'
+
+
+class AccountView(TemplateView):
+    
+    #freeschool_account.htmlをレンダリング（描写）する
+    template_name='freeschool_account.html'
+
+
+class AccountUpdateView(TemplateView):
+    
+    #freeschool_accountupdate.htmlをレンダリング（描写）する
+    template_name='freeschool_accountupdate.html'
+
+
+class PasswordResetView(TemplateView):
+    
+    #freeschool_passwordreset.htmlをレンダリング（描写）する
+    template_name='freeschool_passwordreset.html'
+
+
+class PasswordResetDoneView(TemplateView):
+    
+    #freeschool_passwordresetdone.htmlをレンダリング（描写）する
+    template_name='freeschool_passwordresetdone.html'
+
+
+class UserSearchView(TemplateView):
+    
+    #freeschool_usersearch.htmlをレンダリング（描写）する
+    template_name='freeschool_usersearch.html'
+
+
+class UserDetailView(TemplateView):
+    
+    #freeschool_userdetail.htmlをレンダリング（描写）する
+    template_name='freeschool_userdetail.html'
