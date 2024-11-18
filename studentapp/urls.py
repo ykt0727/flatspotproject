dc1a472428d78a8ea21fd63bfa087357d1b23230
ykt0727 <ymg2365039@stu.o-hara.ac.jp>
2024-11-18 14:23:29 +0900

    不登校生徒側のルーティングが完了しました

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -4,4 +4,19 @@ from . import views
 app_name='studentapp'
 
 #URLパターンを登録するためのリスト
-urlpatterns=[path('',views.IndexView.as_view(),name='index')]
+urlpatterns=[
+    path('top',views.TopView.as_view(),name='top'),
+    path('clublist',views.ClubListView.as_view(),name='clublist'),
+    path('clubdetail',views.ClubDetailView.as_view(),name='clubdetail'),
+    path('clubrequest',views.ClubRequestView.as_view(),name='clubrequest'),
+    path('clubrequestdone',views.ClubRequestDoneView.as_view(),name='clubrequestdone'),
+    path('eventlist',views.EventListView.as_view(),name='eventlist'),
+    path('eventdetail',views.EventDetailView.as_view(),name='eventdetail'),
+    path('eventrequest',views.EventRequestView.as_view(),name='eventrequest'),
+    path('eventrequestdone',views.EventRequestDoneView.as_view(),name='eventrequestdone'),
+    path('bloglist',views.BlogListView.as_view(),name='bloglist'),
+    path('blogdetail',views.BlogDetailView.as_view(),name='blogdetail'),
+    path('contact',views.ContactView.as_view(),name='contact'),
+    path('contactdone',views.ContactDoneView.as_view(),name='contactdone'),
+    path('about',views.AboutView.as_view(),name='about'),
+    ]
--- studentapp/views.py
+++ studentapp/views.py
@@ -1,7 +1,85 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-class IndexView(TemplateView):
+class TopView(TemplateView):
     
-    #index.htmlをレンダリング（描写）する
+    #student_top.htmlをレンダリング（描写）する
     template_name='student_top.html'
+    
+
+class ClubListView(TemplateView):
+    
+    #student_clublist.htmlをレンダリング（描写）する
+    template_name='student_clublist.html'
+    
+    
+class ClubDetailView(TemplateView):
+    
+    #student_clubdetail.htmlをレンダリング（描写）する
+    template_name='student_clubdetail.html'
+
+
+class ClubRequestView(TemplateView):
+    
+    #student_clubrequest.htmlをレンダリング（描写）する
+    template_name='student_clubrequest.html'
+    
+    
+class ClubRequestDoneView(TemplateView):
+    
+    #student_clubrequestdone.htmlをレンダリング（描写）する
+    template_name='student_clubrequestdone.html'
+
+
+class EventListView(TemplateView):
+    
+    #student_eventlist.htmlをレンダリング（描写）する
+    template_name='student_eventlist.html'
+
+
+class EventDetailView(TemplateView):
+    
+    #student_eventdetail.htmlをレンダリング（描写）する
+    template_name='student_eventdetail.html'
+
+
+class EventRequestView(TemplateView):
+    
+    #student_eventrequest.htmlをレンダリング（描写）する
+    template_name='student_eventrequest.html'
+    
+    
+class EventRequestDoneView(TemplateView):
+    
+    #student_eventrequestdone.htmlをレンダリング（描写）する
+    template_name='student_eventrequestdone.html'
+
+
+class BlogListView(TemplateView):
+    
+    #student_bloglist.htmlをレンダリング（描写）する
+    template_name='student_bloglist.html'
+
+
+class BlogDetailView(TemplateView):
+    
+    #student_blogdetail.htmlをレンダリング（描写）する
+    template_name='student_blogdetail.html'
+
+
+class ContactView(TemplateView):
+    
+    #student_contact.htmlをレンダリング（描写）する
+    template_name='student_contact.html'
+
+
+class ContactDoneView(TemplateView):
+    
+    #student_contactdone.htmlをレンダリング（描写）する
+    template_name='student_contactdone.html'
+
+
+class AboutView(TemplateView):
+    
+    #student_about.htmlをレンダリング（描写）する
+    template_name='student_about.html'
