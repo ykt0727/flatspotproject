aee333a471d34cc5df855a98115b8e8a1a89c815
Nagaoka993 <ymg2365013@stu.o-hara.ac.jp>
2024-11-21 09:25:40 +0900

    mailパス消した

--- flatspotproject/settings.py
+++ flatspotproject/settings.py
@@ -138,5 +138,5 @@ DEFAULT_FROM_EMAIL='wenrenzhanggang@gmail.com'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='wenrenzhanggang@gmail.com'
-EMAIL_HOST_PASSWORD='hoykduztrzwmvwyf'
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
